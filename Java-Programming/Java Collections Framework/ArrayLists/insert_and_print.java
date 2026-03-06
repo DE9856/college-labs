ede4acc9046d62059f311bcf4a2f7a94534fb0a9
Deepesh Kumar Kotta <82948768+DE9856@users.noreply.github.com>
2026-03-06 22:37:29 +0530

    Create insert_and_print.java

---
+++ Java-Programming/Java Collections Framework/ArrayLists/insert_and_print.java
@@ -0,0 +1,12 @@
+//Create an ArrayList of integers and add the values 10, 20, and 30. Print the list.
+
+import java.util.*;
+public class test11{
+    public static void main(String args[]){
+        ArrayList <Integer> list = new ArrayList<Integer>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        System.out.println(list);
+    }
+}
