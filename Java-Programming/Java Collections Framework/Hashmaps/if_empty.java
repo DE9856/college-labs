b9bf4a28d72867568c79070781b86bd07ec44434
Deepesh Kumar Kotta <82948768+DE9856@users.noreply.github.com>
2026-03-06 22:34:18 +0530

    Create if_empty.java

---
+++ Java-Programming/Java Collections Framework/Hashmaps/if_empty.java
@@ -0,0 +1,11 @@
+//Write a program to check whether the HashMap is empty.
+
+import java.util.*;
+public class test7{
+    public static void main(String args[]){
+        HashMap <String, Integer> map = new HashMap <String, Integer>();
+        if(map.isEmpty()){
+            System.out.println("It is empty");
+        }
+    }
+}
