ac44fa8bd532fbbb7f8e0eb3e263b4776e070fd7
Deepesh Kumar Kotta <82948768+DE9856@users.noreply.github.com>
2026-04-24 21:08:58 +0530

    Refactor Kruskal's algorithm for clarity and efficiency

--- C-Programming/ADA/Graph/Kruskals_algorithm.c
+++ C-Programming/ADA/Graph/Kruskals_algorithm.c
@@ -5,62 +5,56 @@
 int cost[MAX][MAX], parent[MAX];
 
 int find(int i){
-    while(parent[i] != 0)
-        i = parent[i];
-    return i;
+	while(parent[i]!=0){
+		i = parent[i];
+	}
+	return i;
 }
 
 int uni(int i, int j){
-    if(i != j){
-        parent[j] = i;
-        return 1;
-    }
-    return 0;
+	if(i!=j){
+		parent[j] = i;
+		return 1;
+	}
+	return 0;
 }
 
 int main(){
-    int i, j, a, b, u, v, n, ne = 1;
-    int min, mincost = 0;
+	int i,j,a,b,u,v,min,mincost=0,n,ne=1;
+	printf("Enter the number of vertices: ");
+	scanf("%d", &n);
 
-    printf("Enter number of vertices: ");
-    scanf("%d", &n);
+	for(i=0;i<n;i++)
+		parent[i] = 0;
 
-    for(i = 0; i < n; i++)
-        parent[i] = 0;
+	printf("Enter the cost adjacency matrix: ");
+	for(i=0;i<n;i++){
+		for(j=0;j<n;j++){
+			scanf("%d", &cost[i][j]);
+			if(cost[i][j]==0)
+				cost[i][j] = INF;
+		}
+	}
 
-    printf("Enter the cost adjacency matrix:\n");
-    for(i = 0; i < n; i++){
-        for(j = 0; j < n; j++){
-            scanf("%d", &cost[i][j]);
-            if(cost[i][j] == 0)
-                cost[i][j] = INF;
-        }
-    }
-
-    while(ne < n){
-        for(i = 0, min = INF; i < n; i++){
-            for(j = 0; j < n; j++){
-                if(cost[i][j] < min){
-                    min = cost[i][j];
-                    a = u = i;
-                    b = v = j;
-                }
-            }
-        }
-
-        u = find(u);
-        v = find(v);
-
-        if(uni(u, v)){
-            printf("Edge %d: (%d, %d) = %d\n", ne++, a, b, min);
-            mincost += min;
-        }
-
-        cost[a][b] = cost[b][a] = INF;
-    }
-
-    printf("Minimum Cost: %d\n", mincost);
-    return 0;
+	while(ne<n){
+		for(i=0,min=INF; i<n;i++){
+			for(j=0;j<n;j++){
+				if(i!=j && cost[i][j]<min){
+					min = cost[i][j];
+					a=u=i;
+					b=v=j;
+				}
+			}
+		}
+		u = find(u);
+		v = find(v);
+		if(uni(u,v)){
+			printf("Edge %d: (%d, %d) = %d\n", ne++, a, b, min);
+			mincost+=min;
+		}
+		cost[a][b] = cost[b][a] = INF;
+	}
+	printf("The minimum cost is: %d", mincost);
 }
 
 
